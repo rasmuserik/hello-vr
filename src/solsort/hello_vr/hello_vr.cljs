3312b1465c46db58fbbb539fa006ae6be674e5b2
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-11-06 11:46:19 +0100

    formatting

--- src/solsort/hello_vr/hello_vr.cljs
+++ src/solsort/hello_vr/hello_vr.cljs
@@ -18,12 +18,11 @@
 ;; code for taking down current model
 
 (defn clear-scene []
-(doall
- (for [o js/scene.children]
-   (do (-> o (.-material) (.dispose))
-       (-> o (.-geometry) (.dispose))
-       (.remove js/scene o))
-   )))
+  (doall
+   (for [o js/scene.children]
+     (do (-> o (.-material) (.dispose))
+         (-> o (.-geometry) (.dispose))
+         (.remove js/scene o)))))
 (clear-scene)
 (js/setInterval #(.play js/video) 2000)
 
@@ -36,30 +35,26 @@
 
 (let [canvas (doto (js/document.createElement "canvas")
                (aset "width" 256)
-               (aset "height" 128)
-               )
+               (aset "height" 128))
       frames #js []
       context (doto (.getContext canvas "2d")
-        (aset "fillStyle" "blue")
-        (.fillRect 20,20,100,60)
-        (aset "fillStyle" "red")
-        (aset "font" "30px sans-serif")
-        (.fillText "it works" 10 30)
-        (.drawImage js/video 0 0 256 128)
-        )
+                (aset "fillStyle" "blue")
+                (.fillRect 20,20,100,60)
+                (aset "fillStyle" "red")
+                (aset "font" "30px sans-serif")
+                (.fillText "it works" 10 30)
+                (.drawImage js/video 0 0 256 128))
       texture (doto (js/THREE.Texture. canvas)
                 (aset "needsUpdate" true))
       material (js/THREE.MeshBasicMaterial. #js {:map texture
                                                  :side js/THREE.DoubleSide})
       tv (js/THREE.Mesh.
-              (js/THREE.PlaneGeometry. (.-width canvas) (.-height canvas))
-              material)
+          (js/THREE.PlaneGeometry. (.-width canvas) (.-height canvas))
+          material)
       cube (js/THREE.Mesh.
             (js/THREE.BoxGeometry. 0.5 0.5 0.5)
            ; (js/THREE.MeshNormalMaterial.)
-            material
-            )
-      ]
+            material)]
   (aset js/window "m" material)
   (.set (.-position tv) 0 0 -200)
   (aset (.-position cube) "z" -0.5)
@@ -76,41 +71,31 @@
           (let [timepos (bit-or 0 (* 400 (/ js/video.currentTime js/video.duration)))]
             (js/console.log (aget frames timepos))
             (when (not (aget frames timepos))
-(let [canvas (doto (js/document.createElement "canvas")
-               (aset "width" 256)
-               (aset "height" 128)
-               )
-      context (doto (.getContext canvas "2d")
-        (.drawImage js/video 0 0 256 128)
-        )
-      texture (doto (js/THREE.Texture. canvas)
-                (aset "needsUpdate" true))
-      material (js/THREE.MeshBasicMaterial. #js {:map texture
-                                                 :side js/THREE.DoubleSide})
-      frame (js/THREE.Mesh.
-              (js/THREE.PlaneGeometry. (.-width canvas) (.-height canvas))
-              material)
-      ]
-  (aset frames timepos true)
-  (aset js/window "m" material)
-  (.set (.-position frame) (* 256 (- (rem timepos 20) 10)) (* 128 (- 10 (quot timepos 20))) -1000)
-  (.add js/scene frame)
-  )
-              )
+              (let [canvas (doto (js/document.createElement "canvas")
+                             (aset "width" 256)
+                             (aset "height" 128))
+                    context (doto (.getContext canvas "2d")
+                              (.drawImage js/video 0 0 256 128))
+                    texture (doto (js/THREE.Texture. canvas)
+                              (aset "needsUpdate" true))
+                    material (js/THREE.MeshBasicMaterial. #js {:map texture
+                                                               :side js/THREE.DoubleSide})
+                    frame (js/THREE.Mesh.
+                           (js/THREE.PlaneGeometry. (.-width canvas) (.-height canvas))
+                           material)]
+                (aset frames timepos true)
+                (aset js/window "m" material)
+                (.set (.-position frame) (* 256 (- (rem timepos 20) 10)) (* 128 (- 10 (quot timepos 20))) -1000)
+                (.add js/scene frame)))
   ;          (js/console.log timepos))
-          ))
-  ))
+))))
 
 (js/console.log "helo")
 ;; misc
 
 (.resetPose js/vrDisplay)
 
-
 (defn testFunc
   [para1]
-  (js/console.log "test" para1)
-)
-
-(testFunc 123)
+  (js/console.log "test" para1)) (testFunc 123)
 
